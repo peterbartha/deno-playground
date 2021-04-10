1213ef09e44d1de9a54b64b9562042c36f58ea9e
Peter Bartha <mail@peterbartha.com>
2021-04-11 00:00:41 +0200

    feat(header): add headline actions (e.g. GitHub repo)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import { IconButton } from '@material-ui/core';
+import { GitHub, Help, WbSunny } from '@material-ui/icons';
 import styles from '../styles/Header.module.scss';
 
 const Header = (): JSX.Element => (
@@ -10,6 +12,30 @@ const Header = (): JSX.Element => (
           <h2>Unofficial land for exploring</h2>
         </div>
       </a>
+
+      <div className={styles.placeholder} />
+
+      <IconButton
+        className={styles.iconButton}
+        aria-label="show GitHub repository"
+      >
+        <WbSunny fontSize="large" />
+      </IconButton>
+      <IconButton
+        className={styles.iconButton}
+        aria-label="show GitHub repository"
+        href="https://github.com/peterbartha/deno-playground"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHub fontSize="large" />
+      </IconButton>
+      <IconButton
+        className={styles.iconButton}
+        aria-label="show GitHub repository"
+      >
+        <Help fontSize="large" />
+      </IconButton>
     </nav>
   </header>
 );
